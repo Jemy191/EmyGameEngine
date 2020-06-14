c15e7e87cad366ac893f09fb29c461f56c19eb08
Émilie Collard <emilie@edokiacademy.com>
2020-06-14 19:26:15 -0400

    Update shader and compiler to most recent version

--- EmyTestGame/Assets/Shaders/Compile.bat
+++ EmyTestGame/Assets/Shaders/Compile.bat
@@ -1,5 +1,5 @@
-C:/VulkanSDK/1.1.108.0/Bin/glslangValidator.exe -V Base.vert -o "BaseVert.spv"
-C:/VulkanSDK/1.1.108.0/Bin/glslangValidator.exe -V Base.frag -o "BaseFrag.spv"
-C:/VulkanSDK/1.1.108.0/Bin/glslangValidator.exe -V TextureColor.frag -o "TextureColorFrag.spv"
+C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -V Base.vert -o "BaseVert.spv"
+C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -V Base.frag -o "BaseFrag.spv"
+C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -V TextureColor.frag -o "TextureColorFrag.spv"
 
 pause
