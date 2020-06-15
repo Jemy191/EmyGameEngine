e2bd1d6a73efcf02d5c19ac991aec6a0b7ac4ac1
Émilie Collard <emilie@edokiacademy.com>
2020-06-14 20:08:56 -0400

    forgot some file

--- EmyRenderingEngine/src/Rendering/OpenGL/OpenGLRenderer.cpp
+++ EmyRenderingEngine/src/Rendering/OpenGL/OpenGLRenderer.cpp
@@ -4,19 +4,52 @@
 OpenGLRenderer::OpenGLRenderer(GLFWwindow* window) : Renderer(window)
 {
 	imgui = std::unique_ptr<ImguiOpenGL>(new ImguiOpenGL(window));
+
+	float position[6] =
+	{
+		-0.5f, -0.5f,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glEnableVertexAttribArray(0);
+
+	std::string vertexShader =
+		"#version 330 core\n"
+		"layout(location = 0) in vec4 position;\n"
+		"void main()\n"
+		"{\n"
+		"gl_Position = position;\n"
+		"}\n";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"layout(location = 0) out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+
+	shader = CreateShader(vertexShader, fragmentShader);
+
+	glUseProgram(shader);
+}
+
+OpenGLRenderer::~OpenGLRenderer()
+{
+	glDeleteProgram(shader);
 }
 
 void OpenGLRenderer::Present(GlfwManager* window)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glBegin(GL_TRIANGLES);
-
-	glVertex2f(-0.5f, -0.5f);
-	glVertex2f(0.0f, 0.5f);
-	glVertex2f(0.5f, -0.5f);
-
-	glEnd();
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	dynamic_cast<ImguiOpenGL*>(imgui.get())->Draw();
 
--- EmyRenderingEngine/src/Rendering/OpenGL/OpenGLRenderer.h
+++ EmyRenderingEngine/src/Rendering/OpenGL/OpenGLRenderer.h
@@ -1,11 +1,82 @@
 #pragma once
 #include "Rendering/Renderer.h"
-#include <Rendering\GlfwManager.h>
+#include <Rendering/GlfwManager.h>
+#include "Helper/Log.h"
+#include <fstream>
 
 class OpenGLRenderer : public Renderer
 {
-public:
-    OpenGLRenderer(GLFWwindow* window);
+private:
+	GLuint buffer;
+	GLuint shader;
 
-    void Present(GlfwManager* window) override;
+	static std::vector<char> LoadShader(std::string fileName)
+	{
+		std::ifstream file("Assets/Shaders/" + fileName + ".spv", std::ios::ate | std::ios::binary);
+
+		if (!file.is_open())
+		{
+			Logger::Log(LogSeverity::FATAL_ERROR, "failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+
+		return buffer;
+	}
+
+	static GLuint CompileShader(GLuint type, std::vector<char> source)
+	{
+		GLuint id = glCreateShader(type);
+		glShaderBinary(1, &id, GL_SHADER_BINARY_FORMAT_SPIR_V, source.data(), source.size());
+		glSpecializeShader(id, "main", 0, nullptr, nullptr);
+		//glShaderSource(id, 1, &src, );
+		//glCompileShader(id);
+
+		GLint result;
+		glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+		if (result == GL_FALSE)
+		{
+			GLint lenght;
+			glGetShaderiv(id, GL_INFO_LOG_LENGTH, &lenght);
+			char* message = (char*)alloca(lenght * sizeof(char));
+			glGetShaderInfoLog(id, lenght, &lenght, message);
+
+			Logger::Log("Failed to compile shader");
+			Logger::Log(std::string(message));
+
+			glDeleteShader(id);
+			return 0;
+		}
+
+		return id;
+	}
+
+	static GLuint CreateShader(const std::string& vert, const std::string& frag)
+	{
+		GLuint program = glCreateProgram();
+		GLuint vs = CompileShader(GL_VERTEX_SHADER, LoadShader("TutoGLVert"));
+		GLuint fs = CompileShader(GL_FRAGMENT_SHADER, LoadShader("TutoGLFrag"));
+
+		glAttachShader(program, vs);
+		glAttachShader(program, fs);
+		glLinkProgram(program);
+		glValidateProgram(program);
+
+		glDeleteShader(vs);
+		glDeleteShader(fs);
+
+		return program;
+	}
+
+public:
+	OpenGLRenderer(GLFWwindow* window);
+	~OpenGLRenderer();
+
+	void Present(GlfwManager* window) override;
 };
