28aa06a30a7211d502b973dc486b6a94dc6fb21b
Émilie Collard <emilie@edokiacademy.com>
2020-06-14 20:08:17 -0400

    Changed raw glsl to spir-v

--- EmyTestGame/Assets/Shaders/Compile.bat
+++ EmyTestGame/Assets/Shaders/Compile.bat
@@ -2,4 +2,7 @@ C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -V Base.vert -o "BaseVert.spv"
 C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -V Base.frag -o "BaseFrag.spv"
 C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -V TextureColor.frag -o "TextureColorFrag.spv"
 
+C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -G TutoGL.vert -o "TutoGLVert.spv"
+C:/VulkanSDK/1.2.141.2/Bin/glslangValidator.exe -G TutoGL.frag -o "TutoGLFrag.spv"
+
 pause
